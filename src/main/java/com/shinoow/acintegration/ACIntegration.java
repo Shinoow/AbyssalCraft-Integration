b4963508f7ede6f17a1952e0988849e907286e39
Shinoow <felix@lundquist.ws>
2016-02-13 02:09:19 +0100

    AbyssalCraft Integration 1.4.0
    *Bump to AbyssalCraft 1.9.0-pre-4
    *Bumped the extra Necronomicon info to AbyssalCraftAPI 1.6
    *You can now add/edit/remove Necronomicon Pages/Chapters through
    MineTweaker
    *Now runs on Forge 11.15.1.1741

--- src/main/java/com/shinoow/acintegration/ACIntegration.java
+++ src/main/java/com/shinoow/acintegration/ACIntegration.java
@@ -25,16 +25,17 @@ import org.apache.logging.log4j.Level;
 
 import com.shinoow.abyssalcraft.api.AbyssalCraftAPI;
 import com.shinoow.abyssalcraft.api.necronomicon.NecroData;
-import com.shinoow.abyssalcraft.api.necronomicon.NecroData.PageData;
+import com.shinoow.abyssalcraft.api.necronomicon.NecroData.Chapter;
+import com.shinoow.abyssalcraft.api.necronomicon.NecroData.Page;
 import com.shinoow.acintegration.integrations.minetweaker.ACMT;
 import com.shinoow.acintegration.integrations.projecte.ACPE;
 import com.shinoow.acintegration.integrations.thaumcraft.ACTC;
 
 @Mod(modid = ACIntegration.modid, name = ACIntegration.name, version = ACIntegration.version, dependencies = "required-after:Forge@[forgeversion,);required-after:abyssalcraft@[1.9.0,];after:Thaumcraft", useMetadata = false, guiFactory = "com.shinoow.acintegration.client.config.ACIGuiFactory",
-updateJSON = "https://raw.githubusercontent.com/Shinoow/AbyssalCraft-Integration/master/version.json")
+acceptedMinecraftVersions = "[1.8.9]", updateJSON = "https://raw.githubusercontent.com/Shinoow/AbyssalCraft-Integration/master/version.json")
 public class ACIntegration {
 
-	public static final String version = "1.3.5";
+	public static final String version = "1.4.0";
 	public static final String modid = "acintegration";
 	public static final String name = "AbyssalCraft Integration";
 
@@ -76,12 +77,12 @@ public class ACIntegration {
 	private void registerIntegrations(){
 		if(Loader.isModLoaded("Thaumcraft") && loadTC){
 			AbyssalCraftAPI.registerACIntegration(new ACTC());
-//			ACTC.instance.preInit();
+			//			ACTC.instance.preInit();
 		}
-//		if(Loader.isModLoaded("EE3") && loadEE3){
-//			AbyssalCraftAPI.registerACIntegration(new ACEE3());
-//			ACEE3.instance.preInit();
-//		}
+		//		if(Loader.isModLoaded("EE3") && loadEE3){
+		//			AbyssalCraftAPI.registerACIntegration(new ACEE3());
+		//			ACEE3.instance.preInit();
+		//		}
 		if(Loader.isModLoaded("MineTweaker3") && loadMT)
 			AbyssalCraftAPI.registerACIntegration(new ACMT());
 		if(Loader.isModLoaded("ProjectE") && loadPE)
@@ -100,12 +101,17 @@ public class ACIntegration {
 		ResourceLocation[] tcres = new ResourceLocation[]{new ResourceLocation("acintegration", "textures/necronomicon/tc1.png"),
 				new ResourceLocation("acintegration", "textures/necronomicon/tc2.png"),
 				new ResourceLocation("acintegration", "textures/necronomicon/tc3.png"), null};
-		String[] morphtxt = new String[]{StatCollector.translateToLocal("necro.text.integration.morph")};
 
-		PageData tc = new PageData(4, "Thaumcraft", tcres, tctxt);
-		PageData morph = new PageData(1, "Morph", morphtxt);
+		Chapter tc = new Chapter("thaumcraft", "Thaumcraft", new Page[]{
+				new Page(1, tcres[0], tctxt[0]), new Page(2, tctxt[1]), new Page(3, tcres[1], tctxt[2]),
+				new Page(4, tctxt[3]), new Page(5, tcres[2], tctxt[4]), new Page(5, tctxt[6]),
+				new Page(7, tctxt[6]), new Page(8, tctxt[7])
+		});
+		Chapter morph = new Chapter("morph", "Morph", new Page[]{
+				new Page(1, StatCollector.translateToLocal("necro.text.integration.morph"))
+		});
 
-		NecroData data = new NecroData(name, StatCollector.translateToLocal("necro.text.integration"), tc, morph);
+		NecroData data = new NecroData(modid, name, StatCollector.translateToLocal("necro.text.integration"), tc, morph);
 
 		AbyssalCraftAPI.registerNecronomiconData(data, 0);
 	}
@@ -119,11 +125,11 @@ public class ACIntegration {
 	private static void syncConfig(){
 
 		loadTC = cfg.get(Configuration.CATEGORY_GENERAL, "Thaumcraft Integration", true, "Whether or not to load the Thaumcraft integration. Disabling this will override any other part of said integration.").getBoolean();
-//		loadEE3 = cfg.get(Configuration.CATEGORY_GENERAL, "Equivalent Exchange 3 Integration", true, "Whether or not to load the Equivalent Exchange 3 integration.").getBoolean();
-//		tcItems = cfg.get(Configuration.CATEGORY_GENERAL, "Thaumcraft Items", true, "Wheter or not to add items through the Thaumcraft integration.").getBoolean();
+		//		loadEE3 = cfg.get(Configuration.CATEGORY_GENERAL, "Equivalent Exchange 3 Integration", true, "Whether or not to load the Equivalent Exchange 3 integration.").getBoolean();
+		//		tcItems = cfg.get(Configuration.CATEGORY_GENERAL, "Thaumcraft Items", true, "Wheter or not to add items through the Thaumcraft integration.").getBoolean();
 		loadMT = cfg.get(Configuration.CATEGORY_GENERAL, "MineTweaker 3 Integration", true, "Whether or not to load the MineTweaker 3 integration.").getBoolean();
 		loadPE = cfg.get(Configuration.CATEGORY_GENERAL, "ProjectE Integration", true, "Whether or not to load the ProjectE integration").getBoolean();
-//		tcWarp = cfg.get(Configuration.CATEGORY_GENERAL, "Thaumcraft Warp", true, "Toggles wheter or not to gain additional warp from attacking/being attacked by AbyssalCraft mobs and being inside AbyssalCraft dimensions").getBoolean();
+		//		tcWarp = cfg.get(Configuration.CATEGORY_GENERAL, "Thaumcraft Warp", true, "Toggles wheter or not to gain additional warp from attacking/being attacked by AbyssalCraft mobs and being inside AbyssalCraft dimensions").getBoolean();
 
 		if(cfg.hasChanged())
 			cfg.save();
---
+++ src/main/java/com/shinoow/acintegration/integrations/minetweaker/InternalNecroData.java
@@ -0,0 +1,296 @@
+package com.shinoow.acintegration.integrations.minetweaker;
+
+import java.util.List;
+
+import minetweaker.IUndoableAction;
+import minetweaker.MineTweakerAPI;
+import minetweaker.api.item.IIngredient;
+import minetweaker.api.item.IItemStack;
+import net.minecraft.util.ResourceLocation;
+import stanhebben.zenscript.annotations.ZenClass;
+import stanhebben.zenscript.annotations.ZenMethod;
+
+import com.shinoow.abyssalcraft.api.AbyssalCraftAPI;
+import com.shinoow.abyssalcraft.api.necronomicon.CraftingStack;
+import com.shinoow.abyssalcraft.api.necronomicon.NecroData.Chapter;
+import com.shinoow.abyssalcraft.api.necronomicon.NecroData.Page;
+
+@ZenClass("mods.abyssalcraft.necronomicon.internal")
+public class InternalNecroData {
+
+	@ZenMethod
+	public static void addChapter(String chapteridentifier, String chaptertitle, String necrodataidentifier){
+		Chapter chapter = new Chapter(chapteridentifier, chaptertitle);
+		MineTweakerAPI.apply(new AddChapter(chapter, necrodataidentifier));
+	}
+
+	private static class AddChapter implements IUndoableAction
+	{
+
+		private final Chapter chapter;
+		private final String identifier;
+		private Chapter oldChapter;
+
+		public AddChapter(Chapter chapter, String identifier){
+
+			this.chapter = chapter;
+			this.identifier = identifier;
+		}
+
+		@Override
+		public void apply() {
+
+			for(Chapter chap : AbyssalCraftAPI.internalNDHandler.getInternalNecroData(identifier).getChapters())
+				if(chap.getIdentifier().equals(chapter.getIdentifier())){
+					oldChapter = chap;
+					break;
+				}
+
+			AbyssalCraftAPI.internalNDHandler.addChapter(chapter, identifier);
+		}
+
+		@Override
+		public boolean canUndo() {
+
+			return true;
+		}
+
+		@Override
+		public String describe() {
+
+			return "Adding Necronomicon Chapter " + chapter.getIdentifier() + " to the NecroData " + identifier;
+		}
+
+		@Override
+		public String describeUndo() {
+
+			return "Removing Necronomicon Chapter " + chapter.getIdentifier() + " from the NecroData " + identifier;
+		}
+
+		@Override
+		public Object getOverrideKey() {
+
+			return null;
+		}
+
+		@Override
+		public void undo() {
+
+			if(oldChapter != null){
+				AbyssalCraftAPI.internalNDHandler.addChapter(oldChapter, identifier);
+			} AbyssalCraftAPI.internalNDHandler.removeChapter(identifier, chapter.getIdentifier());
+		}
+	}
+
+	@ZenMethod
+	public static void removeChapter(String chapteridentifier, String necrodataidentifier){
+		MineTweakerAPI.apply(new RemoveChapter(chapteridentifier, necrodataidentifier));
+	}
+
+	private static class RemoveChapter implements IUndoableAction
+	{
+		List<Chapter> removedChapters;
+		private final String chapteridentifier;
+		private final String necrodataidentifier;
+
+		public RemoveChapter(String chapter, String necrodata){
+			chapteridentifier = chapter;
+			necrodataidentifier = necrodata;
+		}
+
+		@Override
+		public void apply() {
+
+			for(Chapter chap : AbyssalCraftAPI.internalNDHandler.getInternalNecroData(necrodataidentifier).getChapters())
+				if(chap.getIdentifier().equals(chapteridentifier)){
+					removedChapters.add(chap);
+					AbyssalCraftAPI.internalNDHandler.removeChapter(necrodataidentifier, chapteridentifier);
+					return;
+				}	
+		}
+
+		@Override
+		public boolean canUndo() {
+
+			return true;
+		}
+
+		@Override
+		public String describe() {
+
+			return "Removing Necronomicon Chapter " + chapteridentifier + " from the NecroData " + necrodataidentifier;
+		}
+
+		@Override
+		public String describeUndo() {
+
+			return "Re-Adding Necronomicon Chapter " + chapteridentifier + " to the NecroData " + necrodataidentifier;
+		}
+
+		@Override
+		public Object getOverrideKey() {
+
+			return null;
+		}
+
+		@Override
+		public void undo() {
+
+			if(removedChapters != null)
+				for(Chapter chap : removedChapters)
+					if(chap != null)
+						AbyssalCraftAPI.internalNDHandler.addChapter(chap, necrodataidentifier);
+		}
+	}
+
+	@ZenMethod
+	public static void addNormalPage(int pageNum, String pagetext, String chapteridentifier, String necrodataidentifier){
+		Page page = new Page(pageNum, pagetext);
+		MineTweakerAPI.apply(new AddPage(page, chapteridentifier, necrodataidentifier));
+	}
+
+	@ZenMethod
+	public static void addItemPage(int pageNum, IItemStack stack, String pagetext, String chapteridentifier, String necrodataidentifier){
+		Page page = new Page(pageNum, ACMT.toStack(stack), pagetext);
+		MineTweakerAPI.apply(new AddPage(page, chapteridentifier, necrodataidentifier));
+	}
+
+	@ZenMethod
+	public static void addImagePage(int pageNum, String resourcepath, String pagetext, String chapteridentifier, String necrodataidentifier){
+		Page page = new Page(pageNum, new ResourceLocation(resourcepath), pagetext);
+		MineTweakerAPI.apply(new AddPage(page, chapteridentifier, necrodataidentifier));
+	}
+
+	@ZenMethod
+	public static void addCraftingPage(int pageNum, IIngredient thing, String pagetext, String chapteridentifier, String necrodataidentifier){
+		Page page = new Page(pageNum, new CraftingStack(ACMT.toObject(thing)), pagetext);
+		MineTweakerAPI.apply(new AddPage(page, chapteridentifier, necrodataidentifier));
+	}
+
+	@ZenMethod
+	public static void addCraftingPage(int pageNum, IIngredient thing, IIngredient[] stuff, String pagetext, String chapteridentifier, String necrodataidentifier){
+		Page page = new Page(pageNum, new CraftingStack(ACMT.toObject(thing), ACMT.toObjects(stuff)), pagetext);
+		MineTweakerAPI.apply(new AddPage(page, chapteridentifier, necrodataidentifier));
+	}
+
+	private static class AddPage implements IUndoableAction{
+
+		private final Page page;
+		private final String chapteridentifier;
+		private final String necrodataidentifier;
+		private Page oldPage;
+
+		public AddPage(Page page, String chapteridentifier, String necrodataidentifier){
+			this.page = page;
+			this.chapteridentifier = chapteridentifier;
+			this.necrodataidentifier = necrodataidentifier;
+		}
+
+		@Override
+		public void apply() {
+
+			for(Chapter chap : AbyssalCraftAPI.internalNDHandler.getInternalNecroData(necrodataidentifier).getChapters())
+				if(chap.getIdentifier().equals(chapteridentifier)){
+					oldPage = chap.getPage(page.getPageNumber());
+					break;
+				}
+
+			AbyssalCraftAPI.internalNDHandler.addPage(page, necrodataidentifier, chapteridentifier);
+		}
+
+		@Override
+		public boolean canUndo() {
+
+			return true;
+		}
+
+		@Override
+		public String describe() {
+
+			return "Adding Page to Necronomicon Chapter " + chapteridentifier + " in the NecroData " + necrodataidentifier;
+		}
+
+		@Override
+		public String describeUndo() {
+
+			return "Removing Page from Necronomicon Chapter " + chapteridentifier + " in the NecroData " + necrodataidentifier;
+		}
+
+		@Override
+		public Object getOverrideKey() {
+
+			return null;
+		}
+
+		@Override
+		public void undo() {
+
+			if(oldPage != null){
+				AbyssalCraftAPI.internalNDHandler.addPage(oldPage, necrodataidentifier, chapteridentifier);
+			}else AbyssalCraftAPI.internalNDHandler.removePage(page.getPageNumber(), necrodataidentifier, chapteridentifier);
+		}
+	}
+
+	@ZenMethod
+	public static void removePage(int pageNum, String chapteridentifier, String necrodataidentifier){
+		MineTweakerAPI.apply(new RemovePage(pageNum, chapteridentifier, necrodataidentifier));
+	}
+
+	private static class RemovePage implements IUndoableAction{
+
+		List<Page> removedPages;
+		private final int pageNum;
+		private final String chapteridentifier;
+		private final String necrodataidentifier;
+
+		public RemovePage(int pageNum, String chapteridentifier, String necrodataidentifier){
+			this.pageNum = pageNum;
+			this.chapteridentifier = chapteridentifier;
+			this.necrodataidentifier = necrodataidentifier;
+		}
+
+		@Override
+		public void apply() {
+
+			for(Chapter chap : AbyssalCraftAPI.internalNDHandler.getInternalNecroData(necrodataidentifier).getChapters())
+				if(chap.getIdentifier().equals(chapteridentifier)){
+					removedPages.add(chap.getPage(pageNum));
+					chap.removePage(pageNum);
+					return;
+				}
+		}
+
+		@Override
+		public boolean canUndo() {
+
+			return true;
+		}
+
+		@Override
+		public String describe() {
+
+			return "Removing Page from Necronomicon Chapter " + chapteridentifier + " in the NecroData " + necrodataidentifier;
+		}
+
+		@Override
+		public String describeUndo() {
+
+			return "Re-Adding Page to Necronomicon Chapter " + chapteridentifier + " in the NecroData " + necrodataidentifier;
+		}
+
+		@Override
+		public Object getOverrideKey() {
+
+			return null;
+		}
+
+		@Override
+		public void undo() {
+
+			if(removedPages != null)
+				for(Page page : removedPages)
+					if(page != null)
+						AbyssalCraftAPI.internalNDHandler.addPage(page, necrodataidentifier, chapteridentifier);
+		}
+	}
+}
