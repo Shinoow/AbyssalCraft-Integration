771d9190d9c0d6229a44597c24bc07ae7aec941f
Shinoow <felix@lundquist.ws>
2020-05-09 23:45:51 +0200

    Improved wildcard handling for Materializer recipes

--- src/main/java/com/shinoow/acintegration/integrations/minetweaker/Materializer.java
+++ src/main/java/com/shinoow/acintegration/integrations/minetweaker/Materializer.java
@@ -2,6 +2,7 @@ package com.shinoow.acintegration.integrations.minetweaker;
 
 import java.util.List;
 
+import com.google.common.base.Predicates;
 import com.google.common.collect.Lists;
 import com.shinoow.abyssalcraft.api.APIUtils;
 import com.shinoow.abyssalcraft.api.AbyssalCraftAPI;
@@ -9,8 +10,11 @@ import com.shinoow.abyssalcraft.api.recipe.Materialization;
 import com.shinoow.abyssalcraft.api.recipe.MaterializerRecipes;
 
 import crafttweaker.IAction;
+import crafttweaker.api.item.IIngredient;
 import crafttweaker.api.item.IItemStack;
 import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+import net.minecraftforge.oredict.OreDictionary;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -18,34 +22,48 @@ import stanhebben.zenscript.annotations.ZenMethod;
 public class Materializer {
 
 	@ZenMethod
-	public static void addMaterialization(IItemStack output, IItemStack[] input){
+	public static void addMaterialization(IIngredient output, IItemStack[] input){
 
-		ItemStack stack = ACMT.toStack(output);
+		Object obj = ACMT.toObject(output);
 		ItemStack[] stacks = new ItemStack[input.length];
 		for(int i = 0; i < stacks.length; i++)
 			stacks[i] = ACMT.toStack(input[i]);
-		ACMTMisc.TASKS.add(new Add(stack, stacks));
+		ACMTMisc.TASKS.add(new Add(obj, stacks));
 	}
 
 	private static class Add implements IAction {
 
-		private ItemStack output;
+		private Object output;
 		private ItemStack[] input;
+		private String outputName;
 
-		public Add(ItemStack output, ItemStack[] input){
+		public Add(Object output, ItemStack[] input){
 			this.output = output;
 			this.input = input;
 		}
 
 		@Override
 		public void apply() {
-			AbyssalCraftAPI.addMaterialization(output, input);
+			if(output instanceof String) {
+				outputName = (String)output;
+			} else if(output instanceof ItemStack) {
+				ItemStack stack = (ItemStack)output;
+				outputName = stack.getDisplayName();
+				if(stack.getHasSubtypes() && stack.getMetadata() == OreDictionary.WILDCARD_VALUE) {
+					NonNullList<ItemStack> list = NonNullList.create();
+					stack.getItem().getSubItems(stack.getItem().getCreativeTab(), list);
+					list.stream().filter(Predicates.not(ItemStack::isEmpty))
+					.forEach(is -> AbyssalCraftAPI.addMaterialization(is, input));
+				} else AbyssalCraftAPI.addMaterialization(stack, input);
+			}
 		}
 
 		@Override
 		public String describe() {
 
-			return String.format("Adding Materialization recipe for %s (input: %s)", output.getDisplayName(), getArrayContent(input));
+			if(outputName == null)
+				return "Failed to add Materialization recipe, invalid output";
+			return String.format("Adding Materialization recipe for %s (input: %s)", outputName, getArrayContent(input));
 		}
 
 		private String getArrayContent(ItemStack[] array){
@@ -74,9 +92,20 @@ public class Materializer {
 		@Override
 		public void apply() {
 			List<Materialization> temp = Lists.newArrayList();
-			for(Materialization mat : MaterializerRecipes.instance().getMaterializationList())
-				if(APIUtils.areStacksEqual(output, mat.output))
-					temp.add(mat);
+			if(output.getMetadata() == OreDictionary.WILDCARD_VALUE && output.getHasSubtypes()) {
+				NonNullList<ItemStack> list = NonNullList.create();
+				output.getItem().getSubItems(output.getItem().getCreativeTab(), list);
+				list.stream().filter(Predicates.not(ItemStack::isEmpty)).forEach(is -> {
+					for(Materialization mat : MaterializerRecipes.instance().getMaterializationList())
+						if(APIUtils.areStacksEqual(is, mat.output))
+							temp.add(mat);
+				});
+			} else {
+				for(Materialization mat : MaterializerRecipes.instance().getMaterializationList())
+					if(APIUtils.areStacksEqual(output, mat.output))
+						temp.add(mat);
+			}
+
 			for(Materialization mat : temp)
 				MaterializerRecipes.instance().getMaterializationList().remove(mat);
 		}
